2430367a72951549ba58d1592759c879ddb3de4f
Richard Clark <richardclark@Lauras-MacBook-Pro.local>
2017-02-14 15:01:38 -0700

    first

--- templates/contacts.js
+++ templates/contacts.js
@@ -1,5 +1,5 @@
 
-  <button type="button" width "60" height="100" onclick="myJohn()">Call John Doe!!</button>
-  <button type="button" width "60" height="100" onclick="myJohn()">Call Jane Doe!!</button>
-  <button type="button" width "60" height="100" onclick="myJohn()">Call Dr. Leo Marvin!!</button>
-  <button type="button" width "60" height="100" onclick="myJohn()">Call Prevention hotline!!</button>
+  <a href="tel:555-555-5555"><button type="button" width "60" height="100" onclick="myJohn()">Call John Doe!!</button></a>
+  <a href="tel:555-555-5555"><button type="button" width "60" height="100" onclick="myJohn()">Call Jane Doe!!</button></a>
+  <a href="tel:555-555-5555"><button type="button" width "60" height="100" onclick="myJohn()">Call Dr. Leo Marvin!!</button></a>
+  <a href="tel:555-555-5555"><button type="button" width "60" height="100" onclick="myJohn()">Call Prevention hotline!!</button></a>
