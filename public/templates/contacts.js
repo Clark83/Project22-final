b9df2373452a8dac4476bcdbd0d99269ae26df16
Richard Clark <richardclark@Lauras-MacBook-Pro.local>
2017-02-23 10:01:24 -0700

    next

--- public/templates/contacts.js
+++ public/templates/contacts.js
@@ -3,3 +3,10 @@
   <a href="tel:555-555-5555"><button type="button" width "60" height="100" onclick="myJohn()">Call Jane Doe!!</button></a>
   <a href="tel:555-555-5555"><button type="button" width "60" height="100" onclick="myJohn()">Call Dr. Leo Marvin!!</button></a>
   <a href="tel:555-555-5555"><button type="button" width "60" height="100" onclick="myJohn()">Call Prevention hotline!!</button></a>
+
+
+  <form enctype="multipart/form-data" action="/upload/image" method="post">
+    <input name="file[]" type="file" />
+    <button class="add_more">Add More Files</button>
+    <input type="submit" value="Upload File" id="upload"/>
+  </form>
